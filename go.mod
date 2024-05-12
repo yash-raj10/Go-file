23a2ea7b2c1df74b39781da1cfaa725341808c8b
yash <yashraj.se10@gmail.com>
2024-05-12 13:27:34 +0530

    s3 config

--- go.mod
+++ go.mod
@@ -3,6 +3,25 @@ module github.com/yash-raj10/Go-files
 go 1.22.3
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.27.13 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.13 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.16.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.3.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.53.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.24.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.7 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -14,6 +33,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("ERROr")
+	}
+
+	// gin app setup
 	r := gin.Default()
 	r.Static("/assets", "./assets" )
 	r.LoadHTMLGlob("templates/*")
+	r.MaxMultipartMemory = 8 << 20
 
+	//s3 Uploader setup
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Printf("error: %v", err)
+	return
+	}
+	client := s3.NewFromConfig(cfg)
+	uploader := manager.NewUploader(client)
+
+
+	// Routes
 	r.GET("/", func(c *gin.Context){
 		c.HTML(http.StatusOK, "index.html", gin.H{
 		})
@@ -22,22 +47,49 @@ func main(){
 		file, err:= c.FormFile("audio")
 		if err != nil {
 			c.HTML(http.StatusOK, "index.html", gin.H{
-				"error":"Failed to Upload audio",
+				"error":"Failed to get the audio",
 			})
+			return
 		} 
 
-		// sve the file
-		err = c.SaveUploadedFile(file, "assets/uploads/"+ file.Filename)
-		if err != nil {
+//-------------------------- save the file--------------------
+
+		// save to disk--
+		// err = c.SaveUploadedFile(file, "assets/uploads/"+ file.Filename)
+		// if err != nil {
+		// 	c.HTML(http.StatusOK, "index.html", gin.H{
+		// 		"error":"Failed to Upload audio",
+		// 	})
+		// } 
+
+		// save to s3-- (open file)
+		f, openErr := file.Open() 
+		if openErr != nil {
+			c.HTML(http.StatusOK, "index.html", gin.H{
+				"error":"Failed to open audio file",
+			})
+			return
+		} 
+
+		// (upload file)
+		result, UploadErr := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+			Bucket: aws.String("go-file"),
+			Key:    aws.String(file.Filename),
+			Body:   f,
+			ACL: "public-read",
+		})
+		if UploadErr != nil {
 			c.HTML(http.StatusOK, "index.html", gin.H{
 				"error":"Failed to Upload audio",
 			})
+			return
 		} 
 
 		// render  the file
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			// passing audio
-			"audio":"/assets/uploads/"+ file.Filename,
+			// "audio": "/assets/uploads/"+ file.Filename,
+			"audio": result.Location,
 		})
 	})
 
